4215ec490df5662a08f352fec0d765f0fc01b196
Rafi Nazamodeen <rafinazamodeen@Rafis-MacBook-Pro.local>
2023-03-04 21:36:10 -0500

    redo PyBank without pandas lib. Add function for DRY code. More comments

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,36 +1,61 @@
 # import necessary libraries
-import pandas as pd
+import csv
 
-# Initialize output file name
+# Initialize output file name and budget data list so that the data is accessible outside 'with' statement
 output_file = 'PyBank.txt'
+budget_data = []
+
+# define a function to find the date of any particular change given:
+# a 'data_set', changes in pnl ('pnl_diffs') and the 'change' we're looking for
+def date_of_pnl_change(data_set, pnl_diffs, change):
+    """
+    Returns the date that the 'change' happened from the 'data_set'
+    
+    Parameters
+    ----------
+    dataset : list
+        data read in from csv
+    pnl_diffs : list
+        diffences (amount of change) in Profits and Losses (PnL)
+    change : int
+        the particular change in PnL you're looking for
+    """
+    
+    # loop through the dates from the data set and changes in PnL together and return
+    # the date only if it matches the particular 'change' in PnL we're looking for.
+    # Since the date is returned as a single element list, take only that element.
+    change_date = [date[0] for date,pnl in zip(data_set[1:],pnl_diffs) if pnl == change][0]
+    return change_date
 
 # load data from provided csv. We know what the data looks like: 2 cols, multiple rows
-bank_data_set = pd.read_csv('budget_data.csv')
-headers = bank_data_set.columns
-date_header = headers[0]
-pnl_header = headers[1]
+with open('budget_data.csv') as csv_file:
+    bank_data_set = csv.reader(csv_file)
+    header = next(bank_data_set)
+    
+    # We loop through each row of the dataset, we set the first column to the date
+    # and the 2nd column to the PnL as an integer
+    budget_data = [[data[0], int(data[1])] for data in bank_data_set]
 
 # The total number of months included in the dataset
-total_months = len(bank_data_set)
+total_months = len(budget_data)
 
 # The net total amount of Profit/Losses over the entire period
-net_total_pnl = sum(bank_data_set[pnl_header])
+pnls = [pnl[1] for pnl in budget_data]
+net_total_pnl = sum(pnls)
 
 # The average of the changes in Profit/Losses over the entire period
-# first get the changes
-changes_in_pnl = bank_data_set.set_index(date_header).diff()
-# NOTE: changes_in_pnl first difference is NaN because it has no previous month to compare to
-# As a result, we compare 1 month less than the total (i.e. total_months - 1)
-total_change_in_pnl = sum(changes_in_pnl[pnl_header][1:])
-average_change_in_pnl = total_change_in_pnl / (total_months - 1)
+#  1. first get the changes by subtracting a copy of the PnLs from the PnLs offset
+#     from the first row since there's no data before the first row/date
+pnl_changes = [x - y for x,y in zip(pnls[1:], pnls)]
+average_change_in_pnl = round(sum(pnl_changes)/len(pnl_changes), 2)
 
 # The greatest increase in profits (date and amount) over the entire period
-max_incr_in_profits = changes_in_pnl.max().values[0]
-date_of_max_incr_in_profits = changes_in_pnl.idxmax().values[0]
+max_incr_in_profits = max(pnl_changes)
+date_of_max_incr_in_profits = date_of_pnl_change(budget_data, pnl_changes, max_incr_in_profits)
 
 # The greatest decrease in losses (date and amount) over the entire period
-max_decr_in_profits = changes_in_pnl.min().values[0]
-date_of_max_decr_in_profits = changes_in_pnl.idxmin().values[0]
+max_decr_in_profits = min(pnl_changes)
+date_of_max_decr_in_profits = date_of_pnl_change(budget_data, pnl_changes, max_decr_in_profits)
 
 # print out stats: 1. stdout, 2. file
 # 0. Create string to be outputted
